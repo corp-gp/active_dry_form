c0bb50cf5797db8eace7489c9f29e3656a36d2bf
Данилин Даниил <d-danilin@groupprice.ru>
2025-04-04 14:15:36 +0300

    fix rubocop

--- spec/active_dry_form/form_helper_spec.rb
+++ spec/active_dry_form/form_helper_spec.rb
@@ -564,11 +564,13 @@ RSpec.describe ActiveDryForm::FormHelper do
       end
     end
 
-    context "when record with composite primary key" do
+    context 'when record with composite primary key' do
       it 'generate form id with composite key' do
-        form = CompositePrimaryKeyForm.new(record: ProductUser.new(product_id: 1, user_id: 1, state: "pending"))
+        form = CompositePrimaryKeyForm.new(record: ProductUser.new(product_id: 1, user_id: 1, state: 'pending'))
         html = context.active_dry_form_for(form) { |f| f.input(:state) }
-        expect(html).to include_html('<form class="active-dry-form" id="new_product_user_1_1" action="https://example.com" accept-charset="UTF-8" method="post">')
+        expect(html).to include_html(<<~HTML)
+          <form class="active-dry-form" id="new_product_user_1_1" action="https://example.com" accept-charset="UTF-8" method="post">
+        HTML
       end
     end
   end
